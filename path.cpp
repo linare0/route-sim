5cc0be4ea7113f5447469678f72b1315a5b10f1d
Renki Sakai <nm@lvds.co>
2017-12-10 20:19:01 +0900

    bug fixes

--- path.cpp
+++ path.cpp
@@ -1,11 +1,11 @@
 #include "path.hpp"
 
-bool Path::Pending::operator<(Pending* a)
-{
-	return dueTime > a->dueTime;
-};
+bool Path::Pending::operator<(const Pending& a)
+const{
+	return dueTime > a.dueTime;
+}
 
-Path::Pending Path::Pending(void* Data,size_t Count,unsigned long DueTime)
+Path::Pending::Pending( void* Data, size_t Count, unsigned long DueTime, NodeId Dest)
 {
 	data = malloc(Count);
 	memcpy(data,Data,Count);
@@ -14,64 +14,64 @@ Path::Pending Path::Pending(void* Data,size_t Count,unsigned long DueTime)
 	dest = Dest;
 }
 
-Path::~Pending()
+Path::Pending::~Pending()
 {
 	free(data);
 }
 
-Path Path::Path()
+Path::Path()
 {
 	delay = 0;
 	currentTime = 0;
 	node[0] = 0;
 	node[1] = 0;
-};
+}
 
-Path Path::Path(NodeId Node1,NodeId Node2,void* BookPtr,unsigned long delay)
+Path::Path( NodeId Node1, NodeId Node2,std::map<NodeId,void(*)(void*,size_t)>* BookPtr, unsigned long Delay)
 {
 	delay = Delay;
 	currentTime = 0;
 	node[0] = Node1;
 	node[1] = Node2;
-};
+}
 
-void Path::setDelay(unsigned long Delay)
+void Path::setDelay( unsigned long Delay)
 {
 	delay = Delay;
-};
+}
 
-void Path::setBookPtr(void* BookPtr)
+void Path::setBookPtr(std::map<NodeId,void(*)(void*,size_t)>* BookPtr)
 {
 	bookPtr = BookPtr;
 }
 
-void Path::connect(NodeId Node1,NodeId Node2)
+void Path::connect( NodeId Node1, NodeId Node2)
 {
-	node[0] = node1;
-	node[1] = node2;
-};
+	node[0] = Node1;
+	node[1] = Node2;
+}
 
 void Path::disconnect()
 {
 	node[0] = 0;
-	node[1] = 1;
-};
+	node[1] = 0;
+}
 
-void Path::transmit(NodeId Src,const void *Data,size_t Count)
+void Path::transmit( NodeId Src, void *Data, size_t Count)
 {
 	if(Src == node[1])
 		awaits.push(Pending(Data,Count,currentTime + delay,node[0]));
 	else if(Src == node[0])
 		awaits.push(Pending(Data,Count,currentTime + delay,node[1]));
-};
+}
 
-void Path::timeElapsed(unsigned long Elapsed)
+void Path::timeElapsed( unsigned long Elapsed)
 {
 	currentTime += Elapsed;
-	while(awaits.top()->dueTime <= currentTime)
+	while(awaits.top().dueTime <= currentTime)
 	{
-		(*(bookPtr->find(awaits.top()->dest)->second))
-			(awaits.top()->data,awaits.top()->count);
+		(*(bookPtr->find(awaits.top().dest)->second))
+			(awaits.top().data,awaits.top().count);
 		awaits.pop();
 	}
-};
+}
--- path.hpp
+++ path.hpp
@@ -14,9 +14,9 @@ private:
 		void* data;
 		size_t count;
 		NodeId dest;
-		Pending(void* Data,size_t Count, unsigned long DueTime,NodeId Dest);
+		Pending( void* Data, size_t Count,  unsigned long DueTime, NodeId Dest);
 		~Pending();
-		bool operator<(Pending* a);
+		bool operator<(const Pending& a)const;
 	};
 	unsigned long delay;
 	unsigned long currentTime;
@@ -25,11 +25,11 @@ private:
 	std::priority_queue<Pending> awaits;
 public:
 	Path();
-	Path(NodeId Node1,NodeId Node2,void* BookPtr,unsigned long Delay);
-	void setDelay(unsigned long Delay);
-	void setBookPtr(void* BookPtr);
-	void connect(NodeId Node1,NodeId Node2);
+	Path( NodeId Node1, NodeId Node2,std::map<NodeId,void(*)(void*,size_t)>* BookPtr, unsigned long Delay);
+	void setDelay( unsigned long Delay);
+	void setBookPtr(std::map<NodeId,void(*)(void*,size_t)>* BookPtr);
+	void connect( NodeId Node1, NodeId Node2);
 	void disconnect();
-	void transmit(NodeId Src,const void *Data,size_t Count);
-	void timeElapsed(unsigned long Elapsed);
+	void transmit( NodeId Src, void *Data, size_t Count);
+	void timeElapsed( unsigned long Elapsed);
 };
